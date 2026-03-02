89fbcad4d1f4a9f720afef291f7caaf43d2d08ee
Katushenko <34375862+Katushenko@users.noreply.github.com>
2026-03-02 11:21:06 +0300

    Update attendance_bot.py

--- attendance_bot.py
+++ attendance_bot.py
@@ -25,3 +25,33 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS attendance
               lesson3_signup BOOLEAN,
               lesson3_visited BOOLEAN)''')
 conn.commit()
+
+class StudentRegistration(StatesGroup):
+    name = State()
+
+@dp.message_handler(commands=['start'])
+async def cmd_start(message: types.Message):
+    await StudentRegistration.name.set()
+    await message.answer("Введите ваше имя:")
+
+@dp.message_handler(state=StudentRegistration.name)
+async def process_name(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['name'] = message.text
+        
+    cursor.execute("INSERT INTO attendance (name) VALUES (?)", (data['name'],))
+    conn.commit()
+    await state.finish()
+    await message.answer("Вы зарегистрированы!")
+
+@dp.message_handler(commands=['signup'])
+async def signup(message: types.Message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add("Занятие 1", "Занятие 2", "Занятие 3")
+    await message.answer("Выберите занятие для записи:", reply_markup=keyboard)
+
+@dp.message_handler(lambda message: message.text in ["Занятие 1", "Занятие 2", "Занятие 3"])
+async def mark_attendance(message: types.Message):
+    lesson = message.text
+    # Логика отметки присутствия
+    await message.answer(f"Вы записаны на {lesson}")
